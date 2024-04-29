13170fc6fb574ade43a250d94d3f4cb651433427
Shubham <shubhambhutada7810@gmail.com>
2024-04-29 16:57:48 +0530

    Added routes through react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import SignIn from "./pages/SignIn.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import Profile from "./pages/Profile.jsx";
+import About from "./pages/About.jsx";
 
 const App = () => {
   return (
-    <div className='text-red-400'>App</div>
-  )
-}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
