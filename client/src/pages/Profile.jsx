0d69cc1c2121699aff540ca78981327aeb390124
Shubham <shubhambhutada7810@gmail.com>
2024-05-14 13:54:23 +0530

    Completed user listing delete functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -29,6 +29,8 @@ const Profile = () => {
   const [fileUploadError, setFileUploadError] = useState(false);
   const [formData, setFormData] = useState({});
   const [updateSuccess, setUpdateSuccess] = useState(false);
+  const [showListingsError, setShowListingsError] = useState(false);
+  const [userListings, setUserListings] = useState([]);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -117,6 +119,36 @@ const Profile = () => {
       dispatch(signOutUserFailure(error.message));
     }
   }
+
+  async function handleShowListings() {
+    try {
+      setShowListingsError(false);
+      const response = await fetch(`/api/user/listings/${currentUser._id}`);
+      const data = await response.json();
+      if (data.success === false) {
+        setShowListingsError(data.error);
+        return;
+      }
+      setUserListings(data);
+    } catch (error) {
+      setShowListingsError(true);
+    }
+  }
+  async function handleListingDelete(listingId) {
+    try {
+      const response = await fetch(`/api/listing/delete/${listingId}`, {
+        method: "DELETE",
+      });
+      const data = response.json();
+      if (data.success === false) {
+        console.log(data.message);
+        return;
+      }
+      setUserListings((prev) =>
+        prev.filter((listing) => listing._id != listingId)
+      );
+    } catch (error) {}
+  }
   return (
     <div className="max-w-lg p-3 mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Profile</h1>
@@ -202,6 +234,53 @@ const Profile = () => {
       <p className="text-green-700 mt-5">
         {updateSuccess ? "User Updated Successfully!" : ""}
       </p>
+      <button
+        className="text-green-700 w-full mb-5"
+        onClick={handleShowListings}
+      >
+        Show Listings
+      </button>
+      {showListingsError && (
+        <p className="text-red-700 text-sm mt-5">Error Showing listings</p>
+      )}
+      {userListings && userListings.length > 0 && (
+        <div className="flex flex-col gap-4">
+          <h1 className="text-center text-slate-700 text-2xl font-semibold underline">
+            Your Listings
+          </h1>
+          {userListings.map((listing) => (
+            <div
+              key={listing._id}
+              className="flex items-center p-3 border rounded-lg justify-between gap-4"
+            >
+              <Link to={`/listing/${listing._id}`}>
+                <img
+                  src={listing.imageUrls[0]}
+                  alt="listing cover"
+                  className="h-20 w-24 object-contain"
+                />
+              </Link>
+              <Link
+                className="text-slate-700 font-semibold flex-1 hover:underline truncate"
+                to={`/listing/${listing._id}`}
+              >
+                <p>{listing.name}</p>
+              </Link>
+              <div className="flex flex-col items-center">
+                <button
+                  onClick={() => handleListingDelete(listing._id)}
+                  className="text-red-700 uppercase font-semibold"
+                >
+                  Delete
+                </button>
+                <button className="text-blue-700 uppercase font-semibold">
+                  Edit
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
--- server/controllers/listing.controller.js
+++ server/controllers/listing.controller.js
@@ -1,4 +1,5 @@
 const listingModel = require("../models/listing.model.js");
+const errorHandler = require("../utils/error.js");
 
 const createListing = async (req, res, next) => {
   try {
@@ -9,4 +10,20 @@ const createListing = async (req, res, next) => {
   }
 };
 
-module.exports = { createListing };
+const deleteListing = async (req, res, next) => {
+  try {
+    const listing = await listingModel.findById(req.params.id);
+    if (!listing) {
+      return next(errorHandler(404, "Listing not found!"));
+    }
+    if (req.user.id != listing.userRef) {
+      return next(errorHandler(401, "You can delete only your listings!"));
+    }
+    await listingModel.findByIdAndDelete(req.params.id);
+    res.status(200).json("Listing deleted successfully!");
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { createListing, deleteListing };
