5117a381a3fa81b7d3fa0638e32543c204b4c841
Shubham <shubhambhutada7810@gmail.com>
2024-05-14 15:56:39 +0530

    Created update user listing api

--- server/controllers/listing.controller.js
+++ server/controllers/listing.controller.js
@@ -26,4 +26,24 @@ const deleteListing = async (req, res, next) => {
   }
 };
 
-module.exports = { createListing, deleteListing };
+const updateListing = async (req, res, next) => {
+  try {
+    const listing = await listingModel.findById(req.params.id);
+    if (!listing) {
+      return next(errorHandler(404, "Listing not found!"));
+    }
+    if (req.user.id != listing.userRef) {
+      return next(errorHandler(401, "You can only update your own listing!"));
+    }
+    const updatedListing = await listingModel.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    res.status(200).json(updatedListing);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { createListing, deleteListing, updateListing };
--- server/routes/listing.route.js
+++ server/routes/listing.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { createListing, deleteListing } = require('../controllers/listing.controller.js');
+const { createListing, deleteListing, updateListing } = require('../controllers/listing.controller.js');
 const verifyToken = require('../utils/verifyUser.js');
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createListing)
 router.delete('/delete/:id', verifyToken, deleteListing)
+router.post('/update/:id', verifyToken, updateListing)
 
 module.exports = router;
